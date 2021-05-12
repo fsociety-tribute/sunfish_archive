a06b278b98aca01ef52c0338ab48bcbf0ef8342e
momojuro <mohkun25@gmail.com>
2021-05-13 00:06:40 +0200

    Let's wipe it all to start again

--- drivers/base/power/power_util.h
+++
@@ -1,5 +0,0 @@
-#ifndef __POWER_UTIL_H__
-#define __POWER_UTIL_H__
-
-static int power_pm_monitor_init(void);
-#endif
--- drivers/thermal/airbrush_thermal.c
+++
@@ -1,5 +0,0 @@
-module_platform_driver();
-
-MODULE_AUTHOR("Ahbong Chang <cw.ahbong@google.com>")
-MODULE_DESCRIPTION("Airbrush thermal driver")
-MODULE_LICENSE("GPL")
