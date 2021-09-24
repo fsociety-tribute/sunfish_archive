Kaz205/android_kernel_google_floral

treewide: Silence verbose logging
Signed-off-by: Kazuki Hashimoto <kaz205@tuta.io>

drivers/mfd/adnc/iaxxx-misc.c
@@ -270,7 +270,7 @@ static int iaxxx_misc_notify(struct notifier_block *nb,
 	struct iaxxx_misc_priv_type *misc_priv =
 		container_of(nb, struct iaxxx_misc_priv_type, nb_core);
 
-	dev_info(misc_priv->dev, "action %lu\n", action);
+	dev_dbg(misc_priv->dev, "action %lu\n", action);
 	switch (action) {
 	/* defined in iaxxx-core.h */
 	case IAXXX_EV_CRASH:

fsociety-tribute/sunfish_archive

drivers/mfd/adnc/iaxxx-misc.c
@@ -270,7 +270,7 @@ static int iaxxx_misc_notify(struct notifier_block *nb,
 	struct iaxxx_misc_priv_type *misc_priv =
 		container_of(nb, struct iaxxx_misc_priv_type, nb_core);
 
-	dev_info(misc_priv->dev, "action %d\n", action);
+	dev_dbg(misc_priv->dev, "action %d\n", action);
 	switch (action) {
 	/* defined in iaxxx-core.h */
 	case IAXXX_EV_CRASH: